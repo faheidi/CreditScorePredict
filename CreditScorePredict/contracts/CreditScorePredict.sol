76a450293c9bd8f8dae2d3f042af10a4ed22b337
Fatemeh Heidari <fatemeh.heidary@gmail.com>
2020-05-02 16:17:36 -0400

    bridge

--- CreditScorePredict/contracts/CreditScorePredict.sol
+++ CreditScorePredict/contracts/CreditScorePredict.sol
@@ -19,11 +19,34 @@ contract CreditScorePredict is usingProvable {
         public
         payable
     {
-        emit LogNewProvableQuery("Provable query in-flight!");
-        provable_query(
-            "URL",
-            "json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.0"
-        );
+        request("json(QmZRjkL4U72XFXTY8MVcchpZciHAwnTem51AApSj6Z2byR).XETHZUSD.c.0",
+                "GET",
+                "https://api.kraken.com/0/public/Ticker?pair=ETHUSD",
+                "{'headers': {'content-type': 'json'}}"
+                );
+        // emit LogNewProvableQuery("Provable query in-flight!");
+        // provable_query(
+        //     "URL",
+        //     "json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.0",{'headers': {'content-type': 'json'},"Postal_Code_" : "H3V1A2"}'
+        // );
+    }
+
+    function request(
+        string memory _query,
+        string memory _method,
+        string memory _url,
+        string memory _kwargs
+    )
+        public
+        payable
+    {
+            emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
+            provable_query("computation",
+                [_query,
+                _method,
+                _url,
+                _kwargs]
+            );
     }
 
     function __callback(
